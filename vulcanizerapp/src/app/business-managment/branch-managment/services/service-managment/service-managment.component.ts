92b310a7a43256eb9cb3c1afa27778ef1693ae93
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-08-24 12:03:45 +0200

    impl logic list of type and size of wheel

--- vulcanizerapp/src/app/business-managment/branch-managment/services/service-managment/service-managment.component.ts
+++ vulcanizerapp/src/app/business-managment/branch-managment/services/service-managment/service-managment.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Services, TypOfServices, WheelType } from 'src/app/model/services';
 
 @Component({
   selector: 'app-service-managment',
   templateUrl: './service-managment.component.html',
-  styleUrls: ['./service-managment.component.css']
+  styleUrls: ['./service-managment.component.css'],
 })
 export class ServiceManagmentComponent implements OnInit {
-  tiresSwapChecked : boolean = true;
-  wheelSwapChecked : boolean = true;
-  tiresSwapAluChecked : boolean = true;
-  tiresSwapAluSizeChecked : boolean = true;
-  tiresSwapSteelChecked : boolean = true;
-  wheelBalanceChecked : boolean = true;
-  straighteningRimsChecked : boolean = true;
+  tiresSwapChecked: boolean = true;
+  wheelSwapChecked: boolean = true;
+  tiresSwapAluChecked: boolean = true;
+  tiresSwapAluSizeChecked: boolean = true;
+  tiresSwapSteelChecked: boolean = true;
+  wheelBalanceChecked: boolean = true;
+  straighteningRimsChecked: boolean = true;
 
-  tiresSwapAluSizeList : string[] = new Array<string>()
+  tiresSwapAluSizeList: Services[] = new Array<Services>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.testAluSize();
@@ -27,4 +28,30 @@ export class ServiceManagmentComponent implements OnInit {
     // this.tiresSwapAluSizeList.push("Test 2");
   }
 
+  addTiresSwapAluSize() {
+    const services: Services = new Services();
+    services.typOfServices = TypOfServices.TIRES_SWAP;
+    services.wheelType = WheelType.ALUMINIUM;
+    services._id = this.tiresSwapAluSizeList.length;
+    console.log(services);
+    this.tiresSwapAluSizeList.push(services);
+  }
+
+  getIndexFromSizeList(list: Services[], searchId: number | null): number {
+    for (let i = 0; i < list.length; i++) {
+      const element = list[i];
+      if (element._id == searchId) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  removeTiresSwapAluSize(service: Services) {
+    const index = this.getIndexFromSizeList(
+      this.tiresSwapAluSizeList,
+      service._id
+    );
+    this.tiresSwapAluSizeList.splice(index, 1);
+  }
 }
---
+++ vulcanizerapp/src/app/model/services.ts
@@ -0,0 +1,25 @@
+import { Time } from '@angular/common';
+
+export class Services {
+    id: string | null = null;
+    _id: number | null = null;
+    price: number | null = null;
+    time: Time | null = null;
+    typOfServices: TypOfServices | null = null;
+    wheelType: WheelType | null = null;
+    sizeFrom: number | null = null;
+    sizeTo: number | null = null;
+}
+
+export enum TypOfServices {
+    TIRES_SWAP,
+    WHEEL_SWAP,
+    WHEEL_BALANCE,
+    STRAIGHTENING_RIMS,
+    CUSTOM,
+}
+
+export enum WheelType {
+    ALUMINIUM,
+    STEEL,
+}
